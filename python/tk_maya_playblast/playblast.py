6e92f665ad5265e84ca9dfd1448e3c759228f14d
Liam Hoflay <Liametc@users.noreply.github.com>
2020-05-11 17:47:11 +0100

    Feature/config settings (#5)
    * working example with combo box to determine model editor templates for playblast window
    
    * Removed drop down in favour of individual menu items via the settings yaml

--- python/tk_maya_playblast/playblast.py
+++ python/tk_maya_playblast/playblast.py
@@ -1,16 +1,8 @@
-import datetime
 import os
 import pprint
-import re
-import shutil
 import subprocess
-import sys
-import traceback
 
-from contextlib import contextmanager
-
-import tank
-from tank.platform.qt import QtCore, QtGui
+from sgtk.platform.qt import QtCore, QtGui
 from .playblast_dialog import PlayblastDialog
 
 import pymel.core as pm
@@ -18,9 +10,9 @@ import maya.cmds as cmds
 import maya.mel as mel
 
 class PlayblastManager(object):
-    __uploadToShotgun = True
-    __createVersion = False
-    __showViewer = True
+    __upload_to_shotgun = True
+    __create_version = False
+    __show_viewer = True
 
     """
     Main playblast functionality
@@ -32,23 +24,28 @@ class PlayblastManager(object):
         self._app = app
         self._context = context if context else self._app.context
 
-    def showDialog(self):
+    def show_dialog(self):
         try:
-            self._app.engine.show_dialog("Playblast %s" % self._app.version,
-                                         self._app, PlayblastDialog, self._app, self)
-        except:
-            traceback.print_exc()
+            self._app.engine.show_dialog(
+                "Playblast %s" % self._app.version,
+                self._app,
+                PlayblastDialog,
+                self._app,
+                self
+            )
+        except Exception:
+            self._app.logger.error("Unable to show dialog.", exc_info=True)
 
-    def doPlayblast(self, **overridePlayblastParams):
+    def do_playblast(self, **override_playblast_params):
         template_work = self._app.get_template("template_work")
         template_shot = self._app.get_template("template_shot")
-        sceneName = pm.sceneName()
-        fields = template_work.get_fields(sceneName)
-        self.shotPlayblastPath = template_shot.apply_fields(fields)
+        scene_name = pm.sceneName()
+        fields = template_work.get_fields(scene_name)
+        shot_playblast_path = template_shot.apply_fields(fields)
 
         # Get value of optional config field "temp_directory". If path is
         # invalid or not absolute, use default tempdir.
-        temp_directory = os.path.normpath( self._app.get_setting("temp_directory", "default") )
+        temp_directory = os.path.normpath(self._app.get_setting("temp_directory", "default"))
         if not os.path.isabs(temp_directory):
             import tempfile
             temp_directory = tempfile.gettempdir()
@@ -57,103 +54,113 @@ class PlayblastManager(object):
         if not os.path.isdir(temp_directory):
             os.mkdir(temp_directory)
         # use the basename of generated names
-        self.localPlayblastPath = os.path.join(temp_directory, os.path.basename(self.shotPlayblastPath))
+        local_playblast_path = os.path.join(temp_directory, os.path.basename(shot_playblast_path))
         # run actual playblast routine
-        self.__createPlayblast(**overridePlayblastParams)
-        self._app.log_info("Playblast for %s succesful" % sceneName)
-        
-    def __createPlayblast(self, **overridePlayblastParams):
-        localPlayblastPath = self.localPlayblastPath
-        
+        self._create_playblast(shot_playblast_path, local_playblast_path, override_playblast_params)
+        self._app.logger.info("Playblast for %s succesful", scene_name)
+
+    def _create_playblast(self, shot_playblast_path, local_playblast_path, override_playblast_params):
+
         # setting playback range
-        originalPlaybackRange = ( pm.playbackOptions( query=True, minTime=True ), 
-                                  pm.playbackOptions( query=True, maxTime=True ))
-        startTime = pm.playbackOptions( query=True, animationStartTime=True )
-        endTime = pm.playbackOptions( query=True, animationEndTime=True )
-        pm.playbackOptions( edit=True, minTime=startTime, maxTime=endTime )
-        
+        original_playback_range = (
+            pm.playbackOptions(query=True, minTime=True),
+            pm.playbackOptions(query=True, maxTime=True)
+        )
+        start_time = pm.playbackOptions(query=True, animationStartTime=True)
+        end_time = pm.playbackOptions(query=True, animationEndTime=True)
+        pm.playbackOptions(edit=True, minTime=start_time, maxTime=end_time)
+
         # get playblast parameters from hook
-        playblastParams = self._app.execute_hook("hook_setup_window", action="playblast_params", data=localPlayblastPath)
+        playblast_params = self._app.execute_hook_method(
+            "hook_setup_window",
+            "get_playblast_params",
+            filename=local_playblast_path
+        )
         # get window and editor parameters from hook
-        createWindow = self._app.execute_hook("hook_setup_window", action='create_window')
-
-        # with the created window, do a playblast
-        with createWindow():
-            playblastParams.update(overridePlayblastParams)
-            playblastSuccessful = False
-            while not playblastSuccessful:
+        with self._app.execute_hook_method("hook_setup_window", "create_window") as model_editor:
+            playblast_params.update(override_playblast_params)
+            playblast_params["editorPanelName"] = model_editor
+            self._app.logger.debug(pprint.pformat(playblast_params))
+            playblast_successful = False
+            while not playblast_successful:
+                visible_huds = []
                 try:
-                    # set required visibleHUDs from hook
-                    visibleHUDs = self._app.execute_hook("hook_setup_window", action="hud_set")
+                    # set required visible_huds from hook
+                    visible_huds = self._app.execute_hook_method("hook_setup_window", "set_hud")
 
-                    resultPlayblastPath = pm.playblast( **playblastParams )
-                    playblastSuccessful = True
-                except RuntimeError, e:
-                    if os.path.exists(localPlayblastPath):
-                        playblastSuccessful = True
+                    pm.playblast(**playblast_params)
+                    playblast_successful = True
+                except RuntimeError as error:
+                    if os.path.exists(local_playblast_path):
+                        playblast_successful = True
                     else:
-                        result = QtGui.QMessageBox.critical(None, u"Playblast Error",
-                                                            unicode(e),
-                                                            QtGui.QMessageBox.Retry | QtGui.QMessageBox.Abort)
+                        result = QtGui.QMessageBox.critical(
+                            None,
+                            u"Playblast Error",
+                            unicode(error),
+                            QtGui.QMessageBox.Retry | QtGui.QMessageBox.Abort
+                       )
                         if result == QtGui.QMessageBox.Abort:
                             self._app.logger.exception("Playblast aborted")
                             return
                 finally:
                     # restore HUD state
-                    self._app.execute_hook("hook_setup_window", action="hud_unset", data=visibleHUDs)
+                    self._app.execute_hook_method("hook_setup_window", "unset_huds", huds=visible_huds)
                     # restore playback range
-                    originalMinTime, originalMaxTime = originalPlaybackRange
-                    pm.playbackOptions( edit=True, minTime=originalMinTime, maxTime=originalMaxTime )
+                    original_min_time, original_max_time = original_playback_range
+                    pm.playbackOptions(edit=True, minTime=original_min_time, maxTime=original_max_time)
 
         # do post playblast process, copy files and other necessary stuff
-        result = self._app.execute_hook("hook_post_playblast", action="copy_file", data=localPlayblastPath)
-        
+        result = self._app.execute_hook_method("hook_post_playblast", "copy_file", source=local_playblast_path)
+
         if result:
             self._app.logger.info("Playblast local file created: %s", result)
 
-        if self.__showViewer:
+        if self.__show_viewer:
             self._app.logger.info("Opening RV...")
-            subprocess.call(["rez", "env" , "rv", "--", "rv", str(localPlayblastPath)])
+            subprocess.call(["rez", "env" , "rv", "--", "rv", str(local_playblast_path)])
 
-        if self.__createVersion:
+        if self.__create_version:
             # register new Version entity in shotgun or update existing version, minimize shotgun data
-            playblast_movie = self.shotPlayblastPath
+            playblast_movie = shot_playblast_path
             project = self._app.context.project
             entity = self._app.context.entity
             task = self._app.context.task
 
-            data = { 'project': project,
-                    'code': os.path.basename(playblast_movie),
-                    'description': 'automatic generated by playblast app',
-                    'sg_path_to_movie': playblast_movie,
-                    'entity': entity,
-                    'sg_task': task,
-                }
-            self._app.log_debug("Version-creation hook data:\n" + pprint.pformat(data))
-            result = self._app.execute_hook("hook_post_playblast", action="create_version", data=data)
-            self._app.log_debug("Version-creation hook result:\n" + pprint.pformat(result))
+            data = {
+                "project": project,
+                "code": os.path.basename(playblast_movie),
+                "description": "automatic generated by playblast app",
+                "sg_path_to_movie": playblast_movie,
+                "entity": entity,
+                "sg_task": task,
+            }
+            self._app.logger.debug("Version-creation hook data:\n%s", pprint.pformat(data))
+            result = self._app.execute_hook_method("hook_post_playblast", "create_version", data=data)
+            self._app.logger.debug("Version-creation hook result:\n%s", pprint.pformat(result))
 
             # upload QT file if creation or update process run succesfully
-            if result and self.__uploadToShotgun:
-                result = self._app.execute_hook("hook_post_playblast", action="upload_movie",
-                                                data={
-                                                    "path": data["sg_path_to_movie"],
-                                                    "project": project,
-                                                    "version_id": result["id"],
-                                                })
+            if result and self.__upload_to_shotgun:
+                result = self._app.execute_hook_method(
+                    "hook_post_playblast",
+                    "upload_movie",
+                    data={
+                        "path": data["sg_path_to_movie"],
+                        "project": project,
+                        "version_id": result["id"],
+                    }
+                )
 
-        self._app.log_info("Playblast finished")
+        self._app.logger.info("Playblast finished")
 
-    def setUploadToShotgun(self, value):
-        self._app.log_debug("Upload to Shotgun set to %s" % value)
-        self.__uploadToShotgun = value
+    def set_upload_to_shotgun(self, value):
+        self._app.logger.debug("Upload to Shotgun set to %s", value)
+        self.__upload_to_shotgun = value
 
-    def setCreateVersion(self, value):
-        self._app.log_debug("Create version set to %s" % value)
-        self.__createVersion = value
+    def set_create_version(self, value):
+        self._app.logger.debug("Create version set to %s", value)
+        self.__create_version = value
 
-    def setShowViewer(self, value):
-        self._app.log_debug("Show viewer set to %s" % value)
-        self.__showViewer = value
-
-    
+    def set_shot_viewer(self, value):
+        self._app.logger.debug("Show viewer set to %s", value)
+        self.__show_viewer = value
--- python/tk_maya_playblast/playblast_dialog.py
+++ python/tk_maya_playblast/playblast_dialog.py
@@ -1,10 +1,10 @@
 
-import tank
+import sgtk
 import os
 import sys
 import threading
 
-from tank.platform.qt import QtCore, QtGui
+from sgtk.platform.qt import QtCore, QtGui
 
 SCALE_OPTIONS = [50, 100]
 
@@ -12,7 +12,7 @@ class PlayblastDialog(QtGui.QWidget):
     """
     Main application dialog window
     """
-    
+
     def __init__(self, app, handler, parent=None):
         """
         Constructor
@@ -24,62 +24,81 @@ class PlayblastDialog(QtGui.QWidget):
         self._handler = handler
 
         self._build_ui()
-        
-        self.__initComponents()
-        
+
+        self._init_components()
+
         # most of the useful accessors are available through the Application class instance
         # it is often handy to keep a reference to this. You can get it via the following method:
-        # self._app = tank.platform.current_bundle()
-        
+        # self._app = sgtk.platform.current_bundle()
+
         # via the self._app handle we can for example access:
         # - The engine, via self._app.engine
         # - A Shotgun API instance, via self._app.shotgun
-        # - A tk API instance, via self._app.tk 
-        
+        # - A tk API instance, via self._app.tk
+
         # lastly, set up our very basic UI
         # self.context.setText("Current Shot: %s" % self._app.context)
-        self.btnPlayblast.clicked.connect(self.doPlayblast)
+        self.btn_playblast.clicked.connect(self.do_playblast)
 
     def _build_ui(self):
+        settings = sgtk.platform.import_framework("tk-framework-shotgunutils", "settings")
+        self._settings = settings.UserSettings(self._app)
         self.resize(468, 67)
         layout = QtGui.QGridLayout(self)
-        self.cmbPercentage = QtGui.QComboBox(self)
-        layout.addWidget(self.cmbPercentage, 0, 0)
-        self.chbCreateVersion = QtGui.QCheckBox("Create New Version", self)
-        self.chbCreateVersion.setChecked(False)
-        layout.addWidget(self.chbCreateVersion, 0, 1)
-        self.chbUploadToShotgun = QtGui.QCheckBox("Upload to Shotgun", self)
-        self.chbUploadToShotgun.setChecked(True)
-        self.chbUploadToShotgun.setEnabled(False)
-        self.chbCreateVersion.toggled.connect(self.chbUploadToShotgun.setEnabled)
-        layout.addWidget(self.chbUploadToShotgun, 0, 2)
-        self.chbShowViewer = QtGui.QCheckBox("Show Viewer", self)
-        self.chbShowViewer.setChecked(True)
-        layout.addWidget(self.chbShowViewer, 0, 3)
-        self.btnPlayblast = QtGui.QPushButton("Playblast", self)
-        self.btnPlayblast.setMinimumSize(450, 0)
-        layout.addWidget(self.btnPlayblast, 1, 0, 1, 4)
+        self.cmb_percentage = QtGui.QComboBox(self)
+        layout.addWidget(self.cmb_percentage, 0, 0)
+        self.chb_create_version = QtGui.QCheckBox("Create New Version", self)
+        version_checked = self._settings.retrieve("create_version", default=False)
+        self.chb_create_version.setChecked(version_checked)
+        layout.addWidget(self.chb_create_version, 0, 1)
+        self.chb_upload_to_shotgun = QtGui.QCheckBox("Upload to Shotgun", self)
+        upload_checked = self._settings.retrieve("upload_version", default=True)
+        self.chb_upload_to_shotgun.setChecked(upload_checked)
+        self.chb_upload_to_shotgun.setEnabled(version_checked)
+        self.chb_create_version.toggled.connect(self.chb_upload_to_shotgun.setEnabled)
+        layout.addWidget(self.chb_upload_to_shotgun, 0, 2)
+        self.chb_show_viewer = QtGui.QCheckBox("Show Viewer", self)
+        viewer_checked = self._settings.retrieve("show_viewer", default=True)
+        self.chb_show_viewer.setChecked(viewer_checked)
+        layout.addWidget(self.chb_show_viewer, 0, 3)
+        self.btn_playblast = QtGui.QPushButton("Playblast", self)
+        self.btn_playblast.setMinimumSize(450, 0)
+        layout.addWidget(self.btn_playblast, 1, 0, 1, 4)
 
-    def __initComponents(self):
+    def _init_components(self):
         # Setting up playblast resolution percentage. Customizable through
         # optional "scale_options" field in app settings.
-        scaleIntList = self._app.get_setting("scale_options", SCALE_OPTIONS)
-        for percentInt in scaleIntList:
-            self.cmbPercentage.addItem( "%d%%" % percentInt, userData=percentInt )
+        scale_int_list = self._app.get_setting("scale_options")
+        for percent_int in sorted(scale_int_list, reverse=True):
+            self.cmb_percentage.addItem("%d%%" % percent_int, userData=percent_int)
+        setting_percent = self._settings.retrieve("percent_scale", default=100)
+        index = self.cmb_percentage.findData(setting_percent)
+        index = index if index > 0 else 0
+        self.cmb_percentage.setCurrentIndex(index)
 
-    def doPlayblast(self):
-        overridePlayblastParams = {}
+    def do_playblast(self):
+        override_playblast_params = {}
 
-        createVersion = self.chbCreateVersion.isChecked()
-        self._handler.setCreateVersion( createVersion )
+        create_version = self.chb_create_version.isChecked()
+        self._handler.set_create_version(create_version)
 
-        uploadToShotgun = self.chbUploadToShotgun.isChecked()
-        self._handler.setUploadToShotgun( uploadToShotgun )
+        upload_to_shotgun = self.chb_upload_to_shotgun.isChecked()
+        self._handler.set_upload_to_shotgun(upload_to_shotgun)
 
-        showViewer = self.chbShowViewer.isChecked()
-        self._handler.setShowViewer( showViewer )
+        show_viewer = self.chb_show_viewer.isChecked()
+        self._handler.set_shot_viewer(show_viewer)
 
-        percentInt = self.cmbPercentage.itemData( self.cmbPercentage.currentIndex() )
-        overridePlayblastParams["percent"] = percentInt
-        self._handler.doPlayblast(**overridePlayblastParams)
+        percent_int = self.cmb_percentage.itemData(self.cmb_percentage.currentIndex())
+        override_playblast_params["percent"] = percent_int
+        self._handler.do_playblast(**override_playblast_params)
 
+    def closeEvent(self, event):
+        create_version = self.chb_create_version.isChecked()
+        self._settings.store("create_version", create_version)
+        upload_to_shotgun = self.chb_upload_to_shotgun.isChecked()
+        self._settings.store("upload_version", upload_to_shotgun)
+        show_viewer = self.chb_show_viewer.isChecked()
+        self._settings.store("show_viewer", show_viewer)
+        percent_int = self.cmb_percentage.itemData(self.cmb_percentage.currentIndex())
+        self._settings.store("percent_scale", percent_int)
+        super(PlayblastDialog, self).closeEvent(event)
